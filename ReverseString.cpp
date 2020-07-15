98ce99f2b93b48d491ad7ec8de1764472f367592
unknown <jeanniehjan@gmail.com>
2020-07-15 16:18:11 +0100

    modified reverseString

--- ReverseString.cpp
+++ ReverseString.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <string>
 #include <vector>
+
 class ReverseString
 {
 public:
-
 	std::string reverseWords(std::string s) {
 		s += " ";
 		std::string reversed = "";
@@ -27,15 +27,19 @@ public:
 		for (auto rit = vec.rbegin(); rit != vec.rend(); ++rit) {
 			reversed += *rit;
 			reversed += " ";
-
 		}
+		reversed.pop_back();
 		return reversed;
 	}
 };
 
 int main() {
 	ReverseString reverseString;
-	std::cout << reverseString.reverseWords("  hello world!  ") << std::endl; // world! hello
+
+	std::cout << reverseString.reverseWords("world! hello") << std::endl; // world! hello
 	std::cout << reverseString.reverseWords("a good   example") << std::endl; // example good a
+	std::cout << reverseString.reverseWords("the sky is blue") << std::endl; // blue is sky the
+
+	return 0;
 }
 
