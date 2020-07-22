24c27f3492c4f23e077a49905663ecea5e08419a
unknown <jeanniehjan@gmail.com>
2020-07-22 11:37:46 +0100

    upgraded ReverseString

--- ReverseString.cpp
+++ ReverseString.cpp
@@ -1,44 +1,34 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
-class ReverseString
+#include <sstream>
+
+static std::string reverseWords(const std::string& s)
 {
-public:
-	std::string reverseWords(std::string s) {
-		s += " ";
-		std::string reversed = "";
-		std::string temp = "";
-		std::vector<std::string> vec = std::vector<std::string>();
+	std::stringstream ss(s);
+	std::vector<std::string> vec = std::vector<std::string>();
 
-		for (std::string::size_type i = 0; i < s.size(); i++) {
-			if (isblank(s[i]) && !temp.empty()) {
-
-				temp.erase(std::remove_if(temp.begin(), temp.end(), isspace), s.end());
-				if (temp == "") continue;
-				vec.push_back(temp);
-				temp = "";
-			}
-			else {
-				temp += s[i];
-			}
-		}
-
-		for (auto rit = vec.rbegin(); rit != vec.rend(); ++rit) {
-			reversed += *rit;
-			reversed += " ";
-		}
-		reversed.pop_back();
-		return reversed;
+	std::string temp;
+	while (std::getline(ss, temp, ' '))
+	{
+		if (!temp.empty())
+			vec.push_back(temp);
 	}
-};
+
+	std::string reversed;
+	for (auto rit = vec.rbegin(); rit != vec.rend(); ++rit)
+		reversed += *rit + " ";
+
+	reversed.pop_back();
+	return reversed;
+}
 
 int main() {
-	ReverseString reverseString;
-
-	std::cout << reverseString.reverseWords("world! hello") << std::endl; // world! hello
-	std::cout << reverseString.reverseWords("a good   example") << std::endl; // example good a
-	std::cout << reverseString.reverseWords("the sky is blue") << std::endl; // blue is sky the
+	std::cout << reverseWords("world! hello") << std::endl; // world! hello
+	std::cout << reverseWords("a good   example") << std::endl; // example good a
+	std::cout << reverseWords("the sky is blue") << std::endl; // blue is sky the
 
 	return 0;
 }
